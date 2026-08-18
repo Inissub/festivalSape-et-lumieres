f3f03b7e60d8c69b7553db3eda7ff8bdc276b36c
Inissub <bussinibsk@gmail.com>
2026-08-18 20:02:10 +0200

    feat: mise en place logique des programmes

--- js/functions.js
+++ js/functions.js
@@ -19,3 +19,32 @@ function decompte(){
 }
 decompte();
 setInterval(decompte, 1000);
+
+let vendredi = document.getElementById('vendredi')
+let samedi = document.getElementById('samedi')
+let dimanche = document.getElementById('dimanche')
+let programme = document.getElementById('mon_programme')
+
+vendredi.addEventListener('click', ()=>{
+    programme.innerHTML = `<ul class="prog_liste"  >
+    <li class="prog_item"><span>18:00</span> <p>Ouverture des portes</p></li>
+    <li class="prog_item"><span>19:00</span> <p>Show Sape, parade des Ambianceurs</p></li>
+    <li class="prog_item"><span>21:00</span> <p>Concert d'ouverture</p></li>
+    </ul>`
+})
+
+samedi.addEventListener('click', ()=>{
+    programme.innerHTML = `<ul class="prog_liste" >
+    <li class="prog_item" ><span>10:00</span> <p>Atelier stylisme & couture</p></li>
+    <li class="prog_item"><span>15:00</span> <p>Défilé des créateurs</p></li>
+    <li class="prog_item"><span>20:00</span> <p>Concert de nuit</p></li>
+    </ul>`
+})
+
+dimanche.addEventListener('click', ()=>{
+    programme.innerHTML = `<ul class="prog_liste"  >
+    <li class="prog_item"><span>11:00</span> <p>Brunch festival</p></li>
+    <li class="prog_item"><span>15:00</span> <p>Art et Culture,  expositions lumière</p></li>
+    <li class="prog_item"><span>19:00</span> <p>Cérémonie de clôture</p></li>
+    </ul>`
+})
